79ff73bb1c5b42cb6744332edacc1840704f318c
matthieu <matthieu.marionneau@cern.ch>
2015-07-03 14:25:03 +0200

    add test file and example for reading the METs

--- PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
+++ PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
@@ -22,12 +22,34 @@ process.options = cms.untracked.PSet(
 
 # How many events to process
 process.maxEvents = cms.untracked.PSet( 
-   input = cms.untracked.int32(100)
+   input = cms.untracked.int32(10000)
 )
 
 ### =====================================================================================================
+usePrivateSQlite =False
+
 from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
 process.GlobalTag.globaltag = 'MCRUN2_74_V9A::All'   # for Simulation #same globalTag
+
+if usePrivateSQlite:
+    from CondCore.DBCommon.CondDBSetup_cfi import *
+    era = 'Summer15_V5_MC'
+    process.jec = cms.ESSource("PoolDBESSource",CondDBSetup,
+                               connect = cms.string('sqlite_file:'+os.path.expandvars('$CMSSW_BASE/src/CMGTools/RootTools/data/jec/'+era+'.db')),
+                               toGet =  cms.VPSet(
+            cms.PSet(
+                record = cms.string("JetCorrectionsRecord"),
+                tag = cms.string("JetCorrectorParametersCollection_"+era+"_AK4PF"),
+                label= cms.untracked.string("AK4PF")
+                ),
+            cms.PSet(
+                record = cms.string("JetCorrectionsRecord"),
+                tag = cms.string("JetCorrectorParametersCollection_"+era+"_AK4PFchs"),
+                label= cms.untracked.string("AK4PFchs")
+                ),
+            )
+                               )
+    process.es_prefer_jec = cms.ESPrefer("PoolDBESSource",'jec')
 
 ### =====================================================================================================
 
@@ -35,7 +57,8 @@ process.GlobalTag.globaltag = 'MCRUN2_74_V9A::All'   # for Simulation #same glob
 # Define the input source
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring([
-"root://eoscms//eos/cms/store/relval/CMSSW_7_4_1/RelValTTbar_13/MINIAODSIM/PU25ns_MCRUN2_74_V9_gensim_740pre7-v1/00000/40D9C504-07ED-E411-811D-00259059642E.root"
+            "root://eoscms//eos/cms/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root",
+            "root://eoscms//eos/cms/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/00C0BECF-6F14-E511-96F8-0025904B739A.root",
     ])
 )
 
@@ -63,7 +86,7 @@ runMETCorrectionsAndUncertainties(process, metType="PF",
 #MET T1+Txy
 runMETCorrectionsAndUncertainties(process, metType="PF",
                                   correctionLevel=["T1","Txy"],
-                                  computeUncertainties=True,
+                                  computeUncertainties=False,
                                   produceIntermediateCorrections=False,
                                   addToPatDefaultSequence=False,
                                   jetCollection="selectedPatJets",
@@ -81,21 +104,12 @@ process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
     compressionLevel = cms.untracked.int32(4),
     compressionAlgorithm = cms.untracked.string('LZMA'),
     eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
-    outputCommands = cms.untracked.vstring( "keep *_ak4PFJets_*_RERUN",
-                                            "keep *_pfMet_*_RERUN",
-                                            "keep *_patPFMetT1Txy_*_RERUN",
-                                            "keep *_patPFMetT1*En*_*_RERUN",
-                                            "keep *_patPFMetT1*Res*_*_RERUN",
-                                            "keep *_patPFMet_*_RERUN",
-                                           # "keep *_*_*_PAT",
-                                            "keep *_patJets_*_*",
+    outputCommands = cms.untracked.vstring( "keep *_patPFMetT1Txy_*_RERUN",
+                                            "keep *_patPFMetT1Txy*En*_*_RERUN",
+                                            "keep *_patPFMetT1Txy*Res*_*_RERUN",
                                             "keep *_slimmedMETs_*_*",
-                                            "keep *_patJetCorrFactors_*_*",
-                                            "keep *_selectedPatJetsForMetT1T2Corr_*_*",
-                                            "keep *_fixedGridRhoFastjetAll_*_*"
-                                            #"keep *_patMETs*_*_RERUN",
-                                           ),
-    fileName = cms.untracked.string('testminiAOD.root'),
+                                            ),
+    fileName = cms.untracked.string('corMETMiniAOD.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('')
