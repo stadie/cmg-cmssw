ffa6f25049fca6a80525b4a68c07a438b5dd4e19
schoefbeck <robert.schoefbeck@cern.ch>
2015-08-06 11:12:44 +0200

    fixing input file, set maxEvents=100

--- PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
+++ PhysicsTools/PatAlgos/test/corMETFromMiniAOD.py
@@ -22,7 +22,7 @@ process.options = cms.untracked.PSet(
 
 # How many events to process
 process.maxEvents = cms.untracked.PSet( 
-   input = cms.untracked.int32(-1)
+   input = cms.untracked.int32(100)
 )
 
 #configurable options =======================================================================
@@ -64,7 +64,7 @@ if usePrivateSQlite:
 # Define the input source
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring([
-            "root://eoscms//eos/cms/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root",
+            "root://eoscms.cern.ch//store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v3/10000/009D49A5-7314-E511-84EF-0025905A605E.root",
     ])
 )
 
